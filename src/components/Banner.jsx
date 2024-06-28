ed3689df8b6e707ae50b501c92dce6cf30cbd492
NegiKareena <159912897+NegiKareena@users.noreply.github.com>
2024-06-28 20:03:37 +0530

    Add files via upload

---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+function Banner() {
+  return (
+   <>
+   <div className="max-w-screen-2x1 container mx-auto md:px-20 px-4 flex flex-col md:flex-row">
+   <div className="w-full w-1/2">
+   <div className="space-y-12 mt-12 md:mt-32">
+   <h1 className="text-4xl font-bold ">Discover the beauty of plants at <span className="text-green-500">ArborAlly</span> – where nature thrives.</h1>
+   <p className="text-xl ">Delivering Plants. Delivering Happiness.
+   At ArborAlly, we believe Plants bring spaces to life & bring life to your spaces. we have a mission to make Beautiful, Healthy & High quality plants accessible to all plant 
+   enthusiasts across the country and be a one stop for all their home gardening needs.</p>
+   </div>
+   </div>
+   <div  className="w-full w-1/2">Right</div>
+   </div>
+   
+   </>
+  )
+}
+
+export default Banner
