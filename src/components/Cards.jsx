d6b56a34ea22af8b3e95babd7e945c58a8410cba
Nivedita Bansal <150543474+nivedita156@users.noreply.github.com>
2024-07-01 12:50:11 +0530

    changes

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 function Cards({ item }) {
   return (
     <>
-      <div>
+      <div className="mt-4 my-3 p-3">
         <div className="card bg-base-100 w-96 shadow-xl">
           <figure>
             <img
@@ -18,9 +18,9 @@ function Cards({ item }) {
               <div className="badge badge-secondary">{item.category}</div>
             </h2>
             <p>{item.title}</p>
-            <div className="card-actions justify-end">
-              <div className="badge badge-outline">Fashion</div>
-              <div className="badge badge-outline">Products</div>
+            <div className="card-actions justify-between">
+              <div className="px-2 py-1 rounded-full border-[2px]">${item.price}</div>
+              <div className=" cursor pointer px-2 py-1 rounded-full border-[2px] hover:bg-pink-500 hover:text-white duration 200">Buy Now</div>
             </div>
           </div>
         </div>
